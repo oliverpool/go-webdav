2693fad9efccf13c1873a641ffe19dd58e90e05c
oliverpool <git@olivier.pfad.fr>
2024-10-11 20:53:32 +0200

    caldavtester

---
+++ caldavtester/caldavtester_test.go
@@ -0,0 +1,57 @@
+package caldavtester
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"os"
+	"os/exec"
+	"testing"
+)
+
+func TestServer(t *testing.T) {
+	// TODO: setup backend and listener
+	ln, err := net.Listen("tcp", "localhost:8008")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer ln.Close()
+
+	s := http.Server{
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// t.Log("HIT", r.URL.String())
+			fmt.Println("HIT", r.URL.String())
+		}),
+	}
+	go s.Serve(ln)
+
+	f, err := os.Create("test.log")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(func() {
+		err := f.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+	cmd := exec.Command("nix", "run", "--offline", "--quiet", "../", "--",
+		"testcaldav.py",
+		// relative to the ccs-caldavtester folder
+		"-x", "scripts/tests/CardDAV",
+		"-s", "../serverinfo.xml",
+		"--stop",
+	)
+	cmd.Dir = "ccs-caldavtester"
+	cmd.Stdout = io.MultiWriter(f, os.Stdout)
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+
+	if err := cmd.Run(); err != nil {
+		// t.Log(stderr.String())
+		t.Fatal(err)
+	}
+
+}
---
+++ caldavtester/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "python2";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = (import nixpkgs {
+        inherit system;
+        config = {
+            permittedInsecurePackages = [
+            "python-2.7.18.8"
+            ];
+        };
+        });
+    in {
+    packages.default = pkgs.python2;
+  });
+}
